403080d6268069ac49718fb140af077864fe67c4
Darko Vukovic <vdarkobar@gmail.com>
2024-11-19 20:31:02 +0100

    Update setup2.sh
    new version, user input

--- setup2.sh
+++ setup2.sh
@@ -4,86 +4,79 @@
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 YELLOW='\033[0;33m'
+WHITE='\033[1;37m'
 NC='\033[0m' # No Color
 
 # Script start
 echo
 echo -e "${GREEN} Proxmox VE:${NC}"
 sleep 1
-echo -e "${GREEN}  Script creates a new LXC container with a non-root user for enhanced security${NC}"
+echo -e "${GREEN} Script creates a new LXC container with a non-root user for enhanced security${NC}"
 echo
 
-# Get storage information
-#storage_list=$(pvesm status | awk 'NR>1 {print NR-1 " " $1}')
-storage_list=$(pvesm status -content vztmpl | awk 'NR>1 {print NR-1 " " $1}')    # new command for testing
-
-if [ -z "$storage_list" ]; then
+# Get storage information for templates
+template_storage_list=$(pvesm status -content vztmpl | awk 'NR>1 {print NR-1 " " $1}')
+if [ -z "$template_storage_list" ]; then
     echo -e "${RED} No storage found or failed to retrieve storage status.${NC}"
     exit 1
 fi
 
-# Display available storage
-echo -e "${YELLOW} Available Template storage:${NC}"
-echo "$storage_list"
 echo
-
-# Ask user where to download the template
+echo -e "${WHITE}[INFO] ${YELLOW}Available Template Storage:${WHITE}"
+echo "$template_storage_list"
+default_template_storage=$(echo "$template_storage_list" | awk 'NR==1 {print $2}')
 while true; do
-    echo -e "${YELLOW} Select the storage by selecting the corresponding number: ${NC}"
-    read template_storage_number
-    template_storage=$(echo "$storage_list" | awk -v num=$template_storage_number '$1 == num {print $2}')
+    echo -ne "${GREEN}Select the template storage [default: $default_template_storage]: ${WHITE}"
+    read -r template_storage_number
+    template_storage_number=${template_storage_number:-1}
+    template_storage=$(echo "$template_storage_list" | awk -v num="$template_storage_number" '$1 == num {print $2}')
     if [ -z "$template_storage" ]; then
-        echo -e "${RED} Invalid selection, please try again.${NC}"
+        echo -e "${WHITE}[ERROR] ${RED}Invalid selection, please try again.${WHITE}"
     else
-        echo -e "${GREEN} Template will be downloaded to:${NC}" $template_storage
+        echo -e "${WHITE}[INFO] ${GREEN}Selected Template Storage:${WHITE} $template_storage"
         break
     fi
 done
 
-echo
-
-# Get storage information
-#storage_list=$(pvesm status | awk 'NR>1 {print NR-1 " " $1}')
-storage_list=$(pvesm status -content images | awk 'NR>1 {print NR-1 " " $1}')    # new command for testing
-
-if [ -z "$storage_list" ]; then
+# Get storage information for rootfs
+rootfs_storage_list=$(pvesm status -content images | awk 'NR>1 {print NR-1 " " $1}')
+if [ -z "$rootfs_storage_list" ]; then
     echo -e "${RED} No storage found or failed to retrieve storage status.${NC}"
     exit 1
 fi
 
-# Display available storage
-echo -e "${YELLOW} Available LXC rootfs storage:${NC}"
-echo "$storage_list"
 echo
-
-# Ask user where to store container rootfs
+echo -e "${WHITE}[INFO] ${YELLOW}Available rootfs Storage:${WHITE}"
+echo "$rootfs_storage_list"
+default_rootfs_storage=$(echo "$rootfs_storage_list" | awk 'NR==1 {print $2}')
 while true; do
-    echo -e "${YELLOW} Select the storage by selecting the corresponding number: ${NC}"
-    read template_rootfs
-    template_storage_rootfs=$(echo "$storage_list" | awk -v num=$template_rootfs '$1 == num {print $2}')
-    if [ -z "$template_storage_rootfs" ]; then
-        echo -e "${RED} Invalid selection, please try again.${NC}"
+    echo -ne "${GREEN}Select the rootfs storage [default: $default_rootfs_storage]: ${WHITE}"
+    read -r rootfs_storage_number
+    rootfs_storage_number=${rootfs_storage_number:-1}
+    rootfs_storage=$(echo "$rootfs_storage_list" | awk -v num="$rootfs_storage_number" '$1 == num {print $2}')
+    if [ -z "$rootfs_storage" ]; then
+        echo -e "${WHITE}[ERROR] ${RED}Invalid selection, please try again.${WHITE}"
     else
-        echo -e "${GREEN} Container rootfs will be stored on:${NC}" $template_storage_rootfs
+        echo -e "${WHITE}[INFO] ${GREEN}Selected rootfs Storage:${WHITE} $rootfs_storage"
         break
     fi
 done
 
-echo
-
-# Get next Container ID
-echo -e "${YELLOW} Retrieving the next available container ID...${NC}"
-container_id=$(pvesh get /cluster/nextid)
-if [ $? -ne 0 ] || [ -z "$container_id" ]; then
-    echo -e "${RED} Failed to retrieve the next available container ID. Please check Proxmox VE cluster status or your network connection.${NC}"
+# Get the next available Container ID
+NEXT_CONTAINER_ID=$(pvesh get /cluster/nextid)
+if [ $? -ne 0 ] || [ -z "$NEXT_CONTAINER_ID" ]; then
+    echo -e "${RED} Failed to retrieve the next available container ID.${NC}"
     exit 1
 fi
 
 echo
-echo -e "${GREEN} Next available container ID is:${NC}" $container_id
-echo
+echo -e "${WHITE}[INFO] ${YELLOW}Next available container ID:${WHITE} $NEXT_CONTAINER_ID"
+echo -ne "${GREEN}Enter Container ID [default: $NEXT_CONTAINER_ID]: ${WHITE}"
+read -r CONTAINER_ID
+CONTAINER_ID="${CONTAINER_ID:-$NEXT_CONTAINER_ID}"
+echo -e "${WHITE}[INFO] ${GREEN}Selected Container ID:${WHITE} $CONTAINER_ID"
 
-# Prompt for hostname with all necessary checks
+# Prompt for hostname
 reserved_names=("localhost" "domain" "local" "host" "broadcasthost" "localdomain" "loopback" "wpad" "gateway" "dns" "mail" "ftp" "web")
 is_reserved_name() {
     local input_name=$1
@@ -96,97 +89,65 @@ is_reserved_name() {
 }
 
 while true; do
-    echo -e "${YELLOW} Please enter a hostname: ${NC}"
-    read hstnme
-
-    if [[ -z "$hstnme" ]]; then
-        echo -e "${RED} Error: Hostname cannot be empty.${NC}"
-        continue
-    fi
-
-    if is_reserved_name "$hstnme"; then
-        echo -e "${RED} Error: '$hstnme' is a reserved name. Please choose a different hostname.${NC}"
-        continue
-    fi
-
-    if ! [[ "$hstnme" =~ ^[a-zA-Z0-9]([a-zA-Z0-9\-]{0,251}[a-zA-Z0-9])?$ ]]; then
-        echo -e "${RED} Error: Invalid hostname format. Hostnames must start and end with a letter or digit and can contain hyphens.${NC}"
-        continue
-    fi
-
     echo
-    echo -e "${GREEN} Hostname${NC} $hstnme ${GREEN}is valid and accepted.${NC}"
-    echo
-    break
+    echo -ne "${WHITE}[INFO] ${YELLOW}Enter hostname for the container:${WHITE} "
+    read -r HOSTNAME
+    if [[ -z "$HOSTNAME" ]]; then
+        echo -e "${WHITE}[ERROR] ${RED}Hostname cannot be empty.${WHITE}"
+        continue
+    fi
+    if is_reserved_name "$HOSTNAME"; then
+        echo -e "${WHITE}[ERROR] ${RED}Invalid hostname. Reserved name.${WHITE}"
+    elif [[ "$HOSTNAME" =~ ^[a-zA-Z0-9]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?$ ]]; then
+        echo -e "${WHITE}[INFO] ${GREEN}Selected Hostname:${WHITE} $HOSTNAME"
+        break
+    else
+        echo -e "${WHITE}[ERROR] ${RED}Invalid hostname format.${WHITE}"
+    fi
 done
 
-# Loop until a valid username is entered
+# Prompt for non-root username
 while true; do
-    echo -e "${YELLOW} Please enter username for a non-root user: ${NC}"
-    read username
-
-    # Check for 'root' username
+    echo
+    echo -ne "${WHITE}[INFO] ${YELLOW}Enter username for a non-root user:${WHITE} "
+    read -r username
     if [ "$username" == "root" ]; then
-        echo
-        echo -e "${RED} Error: 'root' is not an allowed username.${NC}"
-        echo
-        continue
-    fi
-
-    # Validate username: must start with a letter or underscore, followed by letters, digits, dashes, or underscores
-    if [[ "$username" =~ ^[a-z_][a-z0-9_-]*$ ]]; then
-        # Check if user already exists
+        echo -e "${WHITE}[ERROR] ${RED}Username 'root' is not allowed.${WHITE}"
+    elif [[ "$username" =~ ^[a-z_][a-z0-9_-]*$ ]]; then
         if id "$username" &>/dev/null; then
-            echo
-            echo -e "${RED} Error: User '$username' already exists.${NC}"
-            echo
+            echo -e "${WHITE}[ERROR] ${RED}User '$username' already exists.${WHITE}"
         else
-            # Valid user which does not exist, so break the loop
+            echo -e "${WHITE}[INFO] ${GREEN}Selected Username:${WHITE} $username"
             break
         fi
     else
-        echo -e "${RED} Error: Invalid username. Use only lowercase letters, digits, dashes, and underscores, starting with a letter or an underscore.${NC}"
+        echo -e "${WHITE}[ERROR] ${RED}Invalid username. Use lowercase letters, digits, dashes, or underscores.${WHITE}"
     fi
 done
 
-# Loop until a valid password is entered
+# Prompt for password
 while true; do
     echo
-    echo -e "${YELLOW} Please enter a password for user '${username}': ${NC}"
+    echo -ne "${WHITE}[INFO] ${YELLOW}Enter password for user '${username}':${WHITE} "
     read -s password
     echo
-    echo -e "${YELLOW} Please re-enter the password for verification: ${NC}"
+    echo -ne "${WHITE}[INFO] ${YELLOW}Re-enter password for verification:${WHITE} "
     read -s password2
-
-    # Check if passwords match
+    echo
     if [ "$password" != "$password2" ]; then
-        echo
-        echo -e "${RED} Error: Passwords do not match. Please try again.${NC}"
-        continue
-    fi
-
-    # Validate password length and optionally complexity
-    if [ ${#password} -lt 8 ]; then
-        echo
-        echo -e "${RED} Error: Password must be at least 8 characters long.${NC}"
-        echo
+        echo -e "${WHITE}[ERROR] ${RED}Passwords do not match. Please try again.${WHITE}"
+    elif [ ${#password} -lt 8 ]; then
+        echo -e "${WHITE}[ERROR] ${RED}Password must be at least 8 characters long.${WHITE}"
+    elif ! [[ "$password" =~ [0-9] ]] || ! [[ "$password" =~ [^a-zA-Z0-9] ]]; then
+        echo -e "${WHITE}[ERROR] ${RED}Password must contain at least one number and one special character.${WHITE}"
     else
-        # Optionally, check for numeric and special characters
-        if ! [[ "$password" =~ [0-9] ]] || ! [[ "$password" =~ [^a-zA-Z0-9] ]]; then
-            echo -e "${RED} Error: Password must contain at least one number and one special character.${NC}"
-            echo
-        else
-            # Password is valid
-            break
-        fi
+        echo -e "${WHITE}[INFO] ${GREEN}Password set successfully.${WHITE}"
+        break
     fi
 done
 
-echo
-echo -e "${GREEN} Username${NC} '${username}' ${GREEN}and password set successfully.${NC}"
-echo
-
 # Update template list
+echo
 if ! pveam update; then
     echo -e "${RED} Failed to update template list. Please check your network or repository configuration.${NC}"
     exit 1
@@ -195,7 +156,6 @@ fi
 echo
 
 # Retrieve the latest Debian LXC template name
-#latest_debian_template=$(pveam available --section system | grep debian | sort -r | head -n 1 | awk '{print $2}')
 latest_debian_template=$(pveam available --section system | awk '/debian/ {print $2}' | sort -V | tail -n 1)
 
 if [ -z "$latest_debian_template" ]; then
@@ -205,7 +165,7 @@ fi
 
 echo -e "${YELLOW} Downloading${NC} $latest_debian_template ${YELLOW}to${NC} $template_storage${YELLOW}...${NC}"
 echo
-if ! pveam download $template_storage $latest_debian_template; then
+if ! pveam download "$template_storage" "$latest_debian_template"; then
     echo -e "${RED} Failed to download the template. Please check your storage configuration and network connection, and ensure the template is still available in the repository.${NC}"
     exit 1
 fi
@@ -234,25 +194,25 @@ echo
 ################
 
 # Create LXC and add non-root user
-pct create $container_id $template_path \
+pct create $CONTAINER_ID $template_path \
     --arch amd64 \
     --ostype debian \
-    --hostname $hstnme \
+    --hostname $HOSTNAME \
     --unprivileged 1 \
     --features nesting=1 \
     --password $password \
     --ignore-unpack-errors \
     --ssh-public-keys /root/.ssh/authorized_keys \
     --ostype debian \
-    --storage $template_storage_rootfs \
-    --rootfs $template_storage_rootfs:8 \
+    --storage $rootfs_storage \
+    --rootfs $rootfs_storage:8 \
     --cores 4 \
     --memory 4096 \
     --swap 512 \
     --net0 name=eth0,bridge=vmbr0,firewall=1,ip=dhcp \
     --start 1
 sleep 3 && \
-pct exec $container_id -- bash -c "
+pct exec $CONTAINER_ID -- bash -c "
 apt update -y && \
 apt upgrade -y && \
 apt install -y sudo && \
@@ -261,10 +221,10 @@ usermod -aG sudo $username && \
 echo '$username:$password' | chpasswd
 "
 # Extracting LXC IP
-LOCAL_IP=$(pct exec $container_id -- ip addr show eth0 | grep 'inet ' | awk '{print $2}' | cut -d/ -f1)
+LOCAL_IP=$(pct exec $CONTAINER_ID -- ip addr show eth0 | grep 'inet ' | awk '{print $2}' | cut -d/ -f1)
 
 # Setting up LXC description
-echo "description: <div align='center'><a href='http://$LOCAL_IP' target='_blank'><img src='https://github.com/vdarkobar/Home-Cloud/blob/main/shared/rsz_debian-logo.png?raw=true'/><br>$LOCAL_IP</a></div>" >> /etc/pve/lxc/$container_id.conf
+echo "description: <div align='center'><a href='http://$LOCAL_IP' target='_blank'><img src='https://github.com/vdarkobar/Home-Cloud/blob/main/shared/rsz_debian-logo.png?raw=true'/><br>$LOCAL_IP</a></div>" >> /etc/pve/lxc/$CONTAINER_ID.conf
 
 # Rebooting LXC
-pct reboot $container_id
+pct reboot $CONTAINER_ID
